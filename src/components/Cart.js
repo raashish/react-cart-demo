5e3b6bb23e81a2b8d4d2e83b8e5d950f13df5a18
raashish <72740781+raashish@users.noreply.github.com>
2022-10-04 23:55:21 +0530

    2 files modified

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,15 +1,85 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Cart = ({ state, dispatch }) => {
-  return <div
-          styles={{
-            display:"flex",
-            flexDirection:"column",
-            margin:10,
-            backgroundColor: "#ececec",
-            padding:10,
-            width:"20%"
-          }}>Cart</div>;
+  const { cart } = state;
+
+  const [total, setTotal] = useState(0);
+
+  const changeQty = (id, qty) => {
+    dispatch({
+      type: 'CHANGE_CART_QTY',
+      payload: { id, qty },
+    });
+  };
+
+  useEffect(() => {
+    setTotal(
+      cart.reduce(
+        (acc, currValue) => acc + Number(currValue.price) * currValue.qty,
+        0
+      )
+    );
+  }, [cart]);
+
+  return (
+    <div
+      styles={{
+        display: 'flex',
+        flexDirection: 'column',
+        margin: 10,
+        backgroundColor: '#ececec',
+        padding: 10,
+        width: '20%',
+      }}
+    >
+      <b style={{ fontSize: 30, alignSelf: 'center' }}></b>
+      <b style={{ alignSelf: 'center' }}>Subtotal: ${total}</b>
+      {cart.length > 0 ? (
+        cart.map((prod) => (
+          <div
+            key={prod.title}
+            style={{
+              display: 'flex',
+              padding: 10,
+              border: '1px solid grey',
+              margin: 5,
+              justifyContent: 'space-between',
+            }}
+          >
+            <div style={{ display: 'flex', gap: 10 }}>
+              <img
+                src={prod.thumbnail}
+                alt={prod.title}
+                style={{ height: 70, objectFit: 'cover' }}
+              />
+
+              <div
+                style={{
+                  display: 'flex',
+                  justifyContent: 'space-evenly',
+                  flexDirection: 'column',
+                }}
+              >
+                <span>{prod.title}</span>
+                <b>${prod.price}</b>
+              </div>
+            </div>
+            <div style={{ display: 'flex', gap: 10, alignItems: 'center' }}>
+              <button onClick={() => changeQty(prod.id, prod.qty - 1)}>
+                -
+              </button>
+              <span>{prod.qty}</span>
+              <button onClick={() => changeQty(prod.id, prod.qty + 1)}>
+                +
+              </button>
+            </div>
+          </div>
+        ))
+      ) : (
+        <span style={{ padding: 20, alignSelf: 'center' }}>Cart is EMPTY!</span>
+      )}
+    </div>
+  );
 };
 
 export default Cart;
--- src/reducers/cartReducer.js
+++ src/reducers/cartReducer.js
@@ -5,7 +5,17 @@ export const cartReducer = (state, action) => {
     case 'ADD_TO_CART':
       return { ...state, cart: [{ ...action.payload }, ...state.cart] };
     case 'REMOVE_FROM_CART':
-      return {...state, cart: state.cart.filter((c)=>c.id !== action.payload.id)}
+      return {
+        ...state,
+        cart: state.cart.filter((c) => c.id !== action.payload.id),
+      };
+    case 'CHANGE_CART_QTY':
+      return {
+        ...state,
+        cart: state.cart.filter((c) =>
+          c.id === action.payload.id ? (c.qty = action.payload.qty) : c.qty
+        ),
+      };
     default:
       break;
   }
